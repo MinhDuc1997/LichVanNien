296ed2a369b03b03d71daf11177c82f5eaf21651
minhduc1997 <duc2521997@gmail.com>
2018-07-08 01:14:08 +0700

    commit

--- app/src/main/java/com/example/root/lichvannien/activity/MonthActivity.kt
+++ app/src/main/java/com/example/root/lichvannien/activity/MonthActivity.kt
@@ -1,25 +1,32 @@
 package com.example.root.lichvannien.activity
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.CalendarView
 import com.example.root.lichvannien.R
 import com.example.root.lichvannien.modules.RandomOn
-import kotlinx.android.synthetic.main.activity_detail_lunar_calendar.*
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
 import kotlinx.android.synthetic.main.activity_month.*
-import kotlinx.android.synthetic.main.activity_one_day.*
 import java.util.*
+
 
 class MonthActivity : AppCompatActivity() {
 
-    val arrayDrawer = listOf(R.drawable.nen, R.drawable.nen1,
+    private val arrayDrawer = listOf(R.drawable.nen, R.drawable.nen1,
             R.drawable.nen2, R.drawable.nen5,
             R.drawable.nen6, R.drawable.nen7,
             R.drawable.nen8, R.drawable.nen9,
             R.drawable.nen10)
+
+    private val calendar = Calendar.getInstance()
+    val d = calendar.get(Calendar.DAY_OF_MONTH)
+    val m = calendar.get(Calendar.MONTH) + 1
+    val y = calendar.get(Calendar.YEAR)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +51,11 @@ class MonthActivity : AppCompatActivity() {
             }
         }
 
+        calendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
+        calendarView.setDateTextAppearance(R.style.DateTextAppearance)
+        calendarView.setWeekDayTextAppearance(R.style.WeekDayTextAppearance)
+        calendarView.addDecorators(SundayDecorator())
+        calendarView.addDecorators(TodayDecorator())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -66,5 +78,38 @@ class MonthActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         bottom_navigation_month.selectedItemId = R.id.month_item
+    }
+
+    inner class SundayDecorator : DayViewDecorator {
+
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            Log.d("day", day.toString())
+            day.copyTo(calendar)
+            val sunDay = calendar.get(Calendar.DAY_OF_WEEK)
+            return sunDay == Calendar.SUNDAY
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            view.setBackgroundDrawable(resources.getDrawable(R.drawable.sunday))
+        }
+    }
+
+    inner class TodayDecorator : DayViewDecorator {
+
+        val calendar2 = Calendar.getInstance()
+
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            day.copyTo(calendar2)
+
+            Log.d("day", "$d $m $y")
+            val dd = calendar2.get(Calendar.DAY_OF_MONTH)
+            val mm= calendar2.get(Calendar.MONTH) + 1
+            val yy = calendar2.get(Calendar.YEAR)
+            return dd == d && mm == m && yy == y
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            view.setBackgroundDrawable(resources.getDrawable(R.drawable.current_day))
+        }
     }
 }
